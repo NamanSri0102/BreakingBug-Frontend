dcb8db7e5ac3640b74034eb8a7aca6f32eb66e22
Naman Srivastava <139103521+NamanSri0102@users.noreply.github.com>
2024-08-03 15:32:27 +0530

    Update store.js

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,7 +2,7 @@ import { configureStore } from '@reduxjs/toolkit';
 import { userReducer } from './userSlice';
 
 const store = configureStore({
-    reducer={
+    reducer:{ // SYNTEX ERROR IS HERE AS  = WAS USED IN PLACE OF :
         user: userReducer,
     }
 });
